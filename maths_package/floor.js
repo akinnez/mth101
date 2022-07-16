f774ff7a9a96240152719f5176abe31af85be72d
Upnextafrica <upnextafrica@gmail.com>
2022-07-17 00:02:40 +0200

    deleted js files

--- maths_package/floor.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function floor(val) {
-    if (val.toString().indexOf('.') == -1)
-        return val;
-    return parseInt((val.toString()).slice(0, (val.toString()).indexOf('.')));
-}
-exports["default"] = floor;
--- maths_package/sort.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function sort(arr) {
-    return arr.sort(function (a, b) {
-        return a - b;
-    });
-}
-exports["default"] = sort;
