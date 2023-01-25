9c6466f88dcffd1b12e9458648fc9add9614494f
akinnez <oyetunjiidris@gmail.com>
2023-01-25 16:17:09 +0100

    removing abs

--- src/utils/differences.ts
+++ src/utils/differences.ts
@@ -7,7 +7,7 @@ function angularDiff(pos1: number[],pos2:number[]):any {
     if (pos1[1] == pos2[1]){
         return abs(pos1[0] - pos2[0])
     } 
-    return abs([(pos1[0] - pos2[0]),(pos1[1] - pos2[1])])
+    return [(pos1[0] - pos2[0]),(pos1[1] - pos2[1])]
 }
 
 export default angularDiff
